01613926e04af81d37ed3a52dc92a65030e0f27e
TravisTheCoolestGuyInTheUniverse <tstop@highpoint.edu>
2020-12-13 15:55:20 -0500

    created models to allow each video to be part of a single category, and to allow each video to have a single creator. Also created model to allow each user to have their own list of videos.

--- ASMRcity/main/admin.py
+++ ASMRcity/main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from main.models import AsmrVideo
+from main.models import AsmrVideo, AsmrCreator, AsmrCategory, UserVideoListEntry
 
 admin.site.register(AsmrVideo)
 # Register your models here.
+admin.site.register(AsmrCreator)
+admin.site.register(AsmrCategory)
+admin.site.register(UserVideoListEntry)
--- ASMRcity/main/models.py
+++ ASMRcity/main/models.py
@@ -1,13 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+
+class AsmrCategory(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    def __str__(self):
+        return self.title
+
+class AsmrCreator(models.Model):
+    name = models.CharField(max_length=200)
+    dateJoined = models.DateField()
+    def __str__(self):
+        return self.name
 
 class AsmrVideo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     datePublished = models.DateTimeField()
     embed = models.CharField(max_length=1000)
-
+    category = models.ForeignKey(AsmrCategory, verbose_name="Category", default=1, on_delete=models.SET_DEFAULT)
+    creator = models.ForeignKey(AsmrCreator, verbose_name="Creator", default=1, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
+
+class UserVideoListEntry(models.Model):
+    #what I think this will do is when a user is deleted, the corresponding User list entry in the database is deleted.
+    #which makes sense in this scenario. If either the user or the video is deleted there doesn't need to be an entry for a video
+    #in their list.
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    video = models.ForeignKey(AsmrVideo, verbose_name="Video", on_delete=models.CASCADE)
 # Create your models here.
